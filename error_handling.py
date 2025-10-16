4f132f5b269f11c49d97ba9a59ddb6538a861ee9
Hamza Tufail <tufailhamza207@gmail.com>
2025-10-16 23:28:58 +0500

    Add files via upload

---
+++ error_handling.py
@@ -0,0 +1,10 @@
+# file = open("welcome.txt", "w")
+# try:
+#     file.write("Chai aur code")
+
+# finally:
+#     file.close
+
+with open("chai_aur_code.txt", "w") as file:
+    file.write("Chai with python series.")
+    
