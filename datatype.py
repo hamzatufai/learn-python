5fe6661e971a36254706ceeed78f20320de7c8b4
Hamza Tufail <tufailhamza207@gmail.com>
2025-10-16 23:30:24 +0500

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,44 @@
+# print(12 + 12)          # 24
+# print(2.5 * 5)          # 12.5
+# print(2 ** 2)           # 4
+# print(2 ** 100)         # Very large number
+
+
+# import math
+# print(math.pi)          # 3.14159...
+
+# import random
+# print(random.random())                      # Random float between 0 and 1
+# print(random.choice([1, 2, 3, 4]))           # Random element from list
+
+
+# username = "HamzaTufail"
+# print(len(username))        # 11
+# print(username[0])          # 'H'
+# print(username[-1])         # 'l'
+# print(username[1:3])        # 'am'
+
+
+# print(dir(username))
+
+# score ="Hamza"
+# print(score)
+
+
+
+# for c in "Hello World":
+#     print (c)
+
+me ="Hello my name is Hamza"
+# print(me[0])
+# print(me[-1])
+me1 = me.split()
+print(me1)
+ 
+    
+ 
+
+
+
+
+ 
